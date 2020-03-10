18fdd575a463970f1d6f2324c2be33d9862221bf
Erick Luiz Fonseca Lopes <902139@lojasrenner.com.br>
2020-03-10 00:09:41 -0300

    Ajustes nos dados do userscript

--- cronos.user.js
+++ cronos.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         cronos.js
+// @id           Cronos Kairos
+// @name         Cronos Kairos
 // @version      1.0.1
 // @description  JS Extension for Kairos
 // @author       Érick Lopes <si.erickluiz@gmail.com>
 // @source       https://github.com/erickLFLopes/cronos-kairos
 // @match        https://www.dimepkairos.com.br/Dimep/Ponto/UserProfilePonto/*
 // @require      https://raw.githubusercontent.com/erickLFLopes/cronos-kairos/master/bundle.min.js
+// @updateVersion   2
 // @grant        none
 // ==/UserScript==
